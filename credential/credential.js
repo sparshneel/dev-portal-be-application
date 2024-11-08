84526b6d74ac4afd036b6e03148527dc05ad9e70
sparshneel <sparshneel.chanchlani@gmail.com>
2024-11-08 17:46:41 +0530

    add code analysis qodana, fix code issues

--- credential/credential.js
+++ credential/credential.js
@@ -1,4 +1,4 @@
-const { randomBytes } = require('crypto');
+const crypto= require('crypto');
 
 const generate_secret = function generateKey(size = 32, format = 'base64') {
     const buffer = crypto.randomBytes(size);
